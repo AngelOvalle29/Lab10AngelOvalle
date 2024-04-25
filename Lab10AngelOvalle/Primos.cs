4fe4912bc4e804acc195fee234faa1397571e731
MSI <MSI@DESKTOP-O3TLS9V>
2024-04-25 09:19:58 -0600

    Agregar archivos de proyecto.

---
+++ Lab10AngelOvalle/Primos.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab10AngelOvalle
+{
+	public class Primos : Padre
+	{
+		public string profesion {  get; set; }
+
+		public int iggs { get; set; }
+	}
+}
---
+++ Lab10AngelOvalle/Tio.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab10AngelOvalle
+{
+	public class Tio : Padre
+	{
+		public string titulo {  get; set; }
+
+		public int noColegiado { get; set; }
+	}
+}
